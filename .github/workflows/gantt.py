ede16315b597abab7ba3ee4443a8fe87cd2876da
johentsch <johannes.hentschel@epfl.ch>
2021-11-16 23:37:04 +0100

    current workflow

--- .github/workflows/gantt.py
+++ .github/workflows/gantt.py
@@ -265,9 +265,16 @@ def write_gantt_charts(args):
         metadata = score_obj.mscx.metadata
         logger = score_obj.mscx.logger
         last_mn = metadata["last_mn"]
-        globalkey = metadata["annotated_key"]
+        try:
+            globalkey = metadata["annotated_key"]
+        except:
+            logger.warning('Global key is missing in the metadata.')
+            globalkey = '?'
         logger.debug(f"Creating Gantt data for {fname}...")
-        data = make_gantt_data(at)
+        data = make_gantt_data(at, logger=logger)
+        if len(data) == 0:
+            logger.debug(f"Could not create Gantt data for {fname}...")
+            continue
         phrases = get_phraseends(at)
         data.sort_values(args.yaxis, ascending=False, inplace=True)
         logger.debug(f"Making and storing Gantt chart for {fname}...")
