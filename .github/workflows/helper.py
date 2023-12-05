34ca83ee5ea4f63e4dec374792839c0b78a6a1b6
Johannes Hentschel <johannes.hentschel@epfl.ch>
2023-12-06 00:04:47 +0100

    updates workflow to v4.3

--- .github/workflows/helper.py
+++ .github/workflows/helper.py
@@ -1,7 +1,7 @@
 import argparse
 import re
 import os
-def create_new_tag(tag):
+def create_new_tag(tag, update_major):
     if not (re.match(r'^v\d+\.\d+$', tag)):
         raise Exception(f'tag: {tag} is not giving in the correct format e.i v0.0')
     
@@ -11,8 +11,15 @@ def create_new_tag(tag):
     if len(digits_tags) != 2:
         raise Exception(f'tag: {tag} must contain two version digits')
     
-    new_digit = int(digits_tags[1]) + 1
-    return "v" + str(digits_tags[0]) + "." + str(new_digit)
+    major_num = int(digits_tags[0])
+    minor_num = int(digits_tags[1])
+    if update_major:
+        print(f"Label detected to update major version")
+        major_num += 1
+        minor_num = 0
+    else:
+        minor_num += 1
+    return f"v{major_num}.{minor_num}"
 
 def store_tag(tag):
     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
@@ -37,21 +44,39 @@ def main(args):
     if not tag:
         print(f"Warning: a latest release with a tag does not exist in current repository, starting from {new_tag}")
     else:
-        new_tag = create_new_tag(tag)
+        new_tag = create_new_tag(tag,args.update_major_ver)
         print(f"Repository with tag: {tag}, creating a new tag with: {new_tag}")
         update_file_with_tag(".zenodo.json", tag, new_tag)
         update_file_with_tag("CITATION.cff", tag, new_tag)
+        update_file_with_tag("README.md", tag, new_tag)
     store_tag(new_tag)
 
 def run():
     args = parser.parse_args()
     main(args)
 
+
+def str_to_bool(value):
+    if value.lower() == "true":
+        return True
+    elif value.lower() == "false":
+        return False
+    else:
+        raise Exception(
+            f"Error: value {value} as argument is not accepted\n"
+            f"retry with true or false"
+        )
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--tag", type=str,
         help="Require: latest tag",
+        required=True
+    )
+    parser.add_argument(
+        "--update_major_ver", type=str_to_bool,
+        help="Require: boolean to update the major tag number",
         required=True
     )
     run()
